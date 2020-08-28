3e73b861da1eb3995622ec5623fb10430ecb7403
Tomas Fernandez <tfernandez@mulesoft.com>
2020-08-28 13:18:32 -0300

    asdasd

--- shared/src/main/scala/org/yaml/model/DefaultJsonErrorHandler.scala
+++ shared/src/main/scala/org/yaml/model/DefaultJsonErrorHandler.scala
@@ -1,16 +1,18 @@
 package org.yaml.model
 import org.mulesoft.lexer.SourceLocation
 
-private class DefaultJsonErrorHandler extends ParseErrorHandler {
+trait DefaultJsonErrorHandler extends ParseErrorHandler {
 
   private lazy val errorHandler: ParseErrorHandler = ParseErrorHandler.parseErrorHandler
 
   override def handle(location: SourceLocation, e: SyamlException): Unit = e match {
-    case _: DuplicateKeyException => // ignore
+    case _: DuplicateKeyException => onIgnoredException(location, e)
     case _ => errorHandler.handle(location, e)
   }
+
+  protected def onIgnoredException(location: SourceLocation, e: SyamlException): Unit = Unit
 }
 
 object DefaultJsonErrorHandler {
-  def apply(): DefaultJsonErrorHandler = new DefaultJsonErrorHandler()
+  def apply(): DefaultJsonErrorHandler = new DefaultJsonErrorHandler { }
 }
